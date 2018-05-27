8d01dc64c35af959b0c146e6e684abfc75939fd2
Peter Braun <pbraun@redhat.com>
2018-05-27 12:21:15 +0200

    start mongo default plugin

---
+++ plugins/default/mongo/plugin_mongo.cpp
@@ -0,0 +1,1 @@
+#include "plugin_mongo.hpp"
---
+++ plugins/default/mongo/plugin_mongo.hpp
@@ -0,0 +1,10 @@
+#ifndef PLUGIN_MONGO_H
+#define PLUGIN_MONGO_H
+
+#include "../../plugin.hpp"
+
+class PluginMongo : public Plugin {
+public:
+};
+
+#endif // PLUGIN_MONGO_H
